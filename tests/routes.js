702ee8f5e8674a853a637e04e22d663dd237da40
Jesús Germade <jesus@germade.es>
2017-05-13 04:20:23 +0200

    first commit

---
+++ tests/routes.js
@@ -0,0 +1,31 @@
+
+var Router = require('../router'),
+    assert = require('assert');
+
+var router = new Router;
+
+describe('Router', function () {
+
+  it('.path', function  () {
+
+    var ids = [];
+
+    router.route('/foo/:fooId', function (id) {
+      ids.push(id);
+    });
+
+    router.run('/foo/foo');
+    router.run('/foo/bar');
+
+    try{
+      router.run('/foo/bar/excluded');
+    } catch(err) {
+      assert.strictEqual( err.route, '/foo/bar/excluded' );
+      assert.strictEqual( err.missing, true );
+    }
+
+    assert.strictEqual( ids.join(', '), 'foo, bar' );
+
+  });
+
+});
